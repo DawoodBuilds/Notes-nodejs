28f43ac07297af48911201cb5099c075a1f064dd
DawoodBuilds <hm.davidhussain@gmail.com>
2025-12-01 16:13:37 +0500

    Feature: AI Quiz Generation

--- Config/openAI.js
+++ Config/openAI.js
@@ -1,6 +1,8 @@
 import { OpenAI } from "openai/client.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 const openai = new OpenAI({
-  apiKey:
-    "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",
+  apiKey: process.env.api_key,
 });
 export default openai;
